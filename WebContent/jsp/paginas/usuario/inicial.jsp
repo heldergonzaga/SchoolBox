4723f43d7aa35813936e2bdfece026f6b77824a6
Helder da Silva Gonzaga <helder.gonzaga@gmail.com>
2013-10-11 11:52:22 -0300

    Versao Inicial

---
+++ WebContent/jsp/paginas/usuario/inicial.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+
+<h1>Listagem de usuarios</h1>
+
+<table>
+	<thead>
+		<tr>
+			<td>id</td>
+			<td>Nome</td>
+			<td>Data Adesao</td>
+			<td>Descricao</td>
+			<td></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>0</td>
+			<td>Fulano de tal </td>
+			<td>12/10/2014</td>
+			<td>Funcionario da teste</td>
+			<td>
+				<a href="#" class="det">Detalhar</a> 
+				<a href="#" class="editar">editar</a>
+				<a href="#" class="excluir">X</a>
+			</td>
+		</tr>
+	</tbody>
+</table>
---
+++ src/br/com/sb/utilitarios/FiltroUrl.java
@@ -0,0 +1,63 @@
+package br.com.sb.utilitarios;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet de filtro, para segurança e controle de url's de acesso
+ */
+@WebServlet(
+		description = "Filtro de controle geral", 
+		urlPatterns = { 
+				"/*", 
+				"/filtro"
+		})
+public class FiltroUrl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public FiltroUrl() {
+        super();
+    }
+    
+    /**
+     * Metodo que faz todo o processamento de url e filtra a segurança da aplicação.
+     */
+    protected void prossessRequest(HttpServletRequest request, HttpServletResponse response){
+    	
+    	String url = request.getRequestURI();
+    	String template = "template.jsp";
+    	String urlFinal = url;
+    	
+    	//Ferifica a altenticação do usuario em questão...
+    	getAutenticacao(request);
+    		
+    	//Se deslogado manda para a pagina de login
+    	
+    	//Quebra a Url para montar o endereço da pagina
+    	
+    	//testa se a Url da pagina é compativel... caso não reenvia para a pagina de erro. 
+    	
+    	//Chama a pagina template, passando o parametro do 
+    	
+    }
+
+    
+    protected boolean getAutenticacao(HttpServletRequest request){
+    	// faz um if se existe sessão para o usuario.
+    	return true;
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		prossessRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		prossessRequest(request, response);
+	}
+
+}
